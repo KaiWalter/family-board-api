424fc23e3b19037ab316c6ada944566b047d8703
Kai Walter <kai.walter@zeiss.com>
2019-04-13 20:13:46 +0200

    try build with publish

--- health/index.ts
+++ health/index.ts
@@ -3,7 +3,7 @@ import * as os from 'os';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.res = {
-        body: "It's alive! --- " + os.type() + " - " + os.release()
+        body: "It's alive! --- OS :" + os.type() + " - " + os.release()
     };
 };
 
