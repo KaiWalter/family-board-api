6809229c2380fe3365ac41e8a0baadb971a9af12
Kai Walter <kai.walter@outlook.de>
2019-04-13 19:02:59 +0200

    initial local

---
+++ health/index.ts
@@ -0,0 +1,10 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as os from 'os';
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.res = {
+        body: "It's alive! --- " + os.type() + " - " + os.release()
+    };
+};
+
+export default httpTrigger;
